780257e52d83c222dfac5825e5317d1bcce7588b
Matt Wolf <probie93@aol.com>
2014-05-04 20:58:50 -0400

    Added a main image, kind of hate it.....

--- assignment7/penguin/Mainindex.php
+++ assignment7/penguin/Mainindex.php
@@ -1,7 +1,10 @@
 <? $page = basename(__FILE__, '.php'); 
 include 'includes/header.php'?>
 <section id = "main_container">
-	<img src = "img/placeholder.jpg" id = "main_image" alt = "Description of image"/>
+	<h2 id = "logo_penguin">Penguin</h2>
+	<h3 id = "logo_home">Home Improvement</h3>
+	<img src = "img/roof.png" id = "main_roof" alt = "Description of image"/>
+	<img src = "img/LogCrossSectionMedium.png" id = "main_image" alt = "Description of image"/>
 	<p class = "main_description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat massa sed dolor consectetur, eu dapibus eros interdum. Sed vel magna tempor, pharetra est sed, tincidunt urna. Cras et velit imperdiet magna lobortis sollicitudin ut et lacus. In scelerisque elit ligula, id cursus augue vestibulum ut. Phasellus quis odio ultricies, semper tortor volutpat, vulputate tortor. Fusce eu imperdiet mi. Morbi interdum arcu at dui mollis, sed vestibulum sem varius. Sed in tincidunt velit, at hendrerit augue. Proin interdum, orci nec tempus vestibulum, massa arcu iaculis tellus, quis posuere mauris lectus vitae ligula. Pellentesque nec elit ut ante tincidunt viverra. Suspendisse ultricies, enim ut iaculis egestas, neque libero aliquet nisi, nec aliquam diam elit at nisi. Vestibulum sit amet rutrum tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed vitae tristique </p>	
 	<article id = "show_off_holder">
 		<section class = "show_off_container">
